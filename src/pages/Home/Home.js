34f9b6acc5641e1fc742c7041b5e31932935b3d3
Jafar Iqbal Choudhury <jafariqbalchoudhury@gmail.com>
2022-12-14 10:32:27 +0530

    Sales Table Complete
    Here the home page is complete which contains -
    Widgets
    Charts
    Tables
    Sidebar
    and Navbar

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,6 +4,7 @@ import Navbar from "../../components/Navbar/Navbar";
 import Widgets from "../../components/Widgets/Widgets";
 import FeaturedChart from "../../components/FeaturedChart/FeaturedChart";
 import NormalChart from "../../components/NormalChart/NormalChart";
+import Tables from "../../components/Table/Table";
 
 const Home = () => {
     return (
@@ -20,6 +21,10 @@ const Home = () => {
                 <div className="home__charts">
                     <FeaturedChart/>
                     <NormalChart/>
+                </div>
+                <div className="home__listContainer">
+                    <div className="home__listTitle">Latest Transactions </div>
+                    <Tables/>
                 </div>
             </div>
         </div>
